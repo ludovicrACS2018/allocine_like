59950cdf4e03072bbf5ab8e8ffab31c0fa727209
Draker21 <drak3r21@hotmail.com>
2018-05-24 14:48:22 +0200

    Ajout du composer, twig et de la mise en application

--- index.php
+++ index.php
@@ -1,41 +1,18 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <link rel="stylesheet" href="style.css">
-    <title>Allocine Like</title>
-</head>
-<body>
-<div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer
-            mdl-layout--fixed-header">
-  <header class="mdl-layout__header">      <nav class="mdl-navigation">
-        <a class="mdl-navigation__link" href="#">Home</a>
-        <a class="mdl-navigation__link" href="#">Blog</a>
-        <a class="mdl-navigation__link" href="#">About</a>
-        <a class="mdl-navigation__link" href="#">Contact</a>
-      </nav>
-    <div class="mdl-layout__header-row">
-      <div class="mdl-layout-spacer"></div>
-      <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
-                  mdl-textfield--floating-label mdl-textfield--align-right">
-        <label class="mdl-button mdl-js-button mdl-button--icon"
-               for="fixed-header-drawer-exp">
-          <i class="material-icons">search</i>
-        </label>
-        <div class="mdl-textfield__expandable-holder">
-          <input class="mdl-textfield__input" type="text" name="sample"
-                 id="fixed-header-drawer-exp">
-        </div>
-      </div>
-    </div>
-  </header>
- 
-</div>
-    <script src="script.js"></script>
-</body>
-</html>
+<?php require 'vendor/autoload.php';?>
+<?=
+// Routing
+$page = 'home';
+if (isset($_GET['p'])){
+    $page = $_GET['p'];
+}
+//Rendu du template
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates'); //précise dans quel dossier twig doit aller chercher les templates (départ du dossier actuel à /templates)
+//On initiliase maintenant Twig
+$twig = new Twig_Environment($loader,[
+    'cache' => __DIR__ . '/tmp',
+    'auto_reload' => true,
+]);
+
+if($page === 'home'){
+    echo $twig->render('home.twig');
+}
